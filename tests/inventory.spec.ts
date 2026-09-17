7931b9bde0c8f735dab8370720d85416c343f935
jmhc1990 <jmhc.90@hotmail.com>
2026-09-17 12:07:50 +0200

    refactor: implement custom fixtures for page objects injection

--- tests/inventory.spec.ts
+++ tests/inventory.spec.ts
@@ -1,26 +1,15 @@
-import { test } from '@playwright/test';
-import { LoginPage } from '../pages/LoginPage';
-import { InventoryPage } from '../pages/InventoryPage';
+import { test, expect } from './fixtures';
 
 test.describe('Flujo de Catálogo - SauceDemo', () => {
-  let loginPage: LoginPage;
-  let inventoryPage: InventoryPage;
 
-  test.beforeEach(async ({ page }) => {
-    loginPage = new LoginPage(page);
-    inventoryPage = new InventoryPage(page);
-
-    // Precondición: Estar autenticado en la plataforma
+  test.beforeEach(async ({ loginPage, inventoryPage }) => {
     await loginPage.goto();
     await loginPage.login('standard_user', 'secret_sauce');
     await inventoryPage.expectLoaded();
   });
 
-  test('Debería agregar un producto al carrito correctamente', async () => {
-    // 1. Añadir el primer producto
+  test('Debería agregar un producto al carrito correctamente', async ({ inventoryPage }) => {
     await inventoryPage.addFirstItemToCart();
-
-    // 2. Validar que el badge del carrito cambie a '1'
     await inventoryPage.expectCartCount('1');
   });
 });
